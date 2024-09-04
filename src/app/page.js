4b7f1050959b666cbbc86f88b903dc91e9dd043e
jhs512 <jangka512@gmail.com>
2024-09-05 00:41:52 +0900

    work

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,13 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
 export default function Home() {
-  return <div>안녕</div>;
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setCount(Math.floor(Math.random() * 100));
+  }, []);
+
+  return <div>{count}</div>;
 }
